6128d07bea594d9e3e92d7216bb23513d8666ab3
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-08 19:52:04 +0530

    Create necessary pages and their css files

---
+++ Frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../Styles/HomePage.css";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ Frontend/src/Pages/SearchBook.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../Styles/SearchBook.css";
+
+function SearchBook() {
+  return (
+    <div>
+      <h1>Search book</h1>
+    </div>
+  );
+}
+
+export default SearchBook;
