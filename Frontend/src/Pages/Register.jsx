7c3532832c293b697b2b4746b0dc6447159c37b4
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-10 18:59:41 +0530

    complete Register Page

--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -1,10 +1,105 @@
 import React from "react";
+import { useState } from "react";
+import { validate } from "./../Validation/RegisterValidation";
 import "./../Styles/RegisterPage.css";
+import Register_Img from "./../Images/Register_Image.jpg";
 
 function Register() {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    idCardNumber: "",
+    phoneNumber: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { isValid, newErrors } = validate(userData);
+    if (isValid) {
+      console.log(userData);
+      setUserData({
+        username: "",
+        email: "",
+        password: "",
+        idCardNumber: "",
+        phoneNumber: "",
+      });
+      setErrors({});
+    } else {
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register-container">
+      <div className="register-left">
+        <img src={Register_Img} alt="" />
+      </div>
+      <div className="register-right">
+        <h1>Register</h1>
+        <input
+          className={errors.username ? "error-input" : ""}
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={userData.username}
+          onChange={handleChange}
+        />
+        {errors.username && <p className="error">{errors.username}</p>}
+        <input
+          className={errors.email ? "error-input" : ""}
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={userData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <p className="error">{errors.email}</p>}
+        <input
+          className={errors.password ? "error-input" : ""}
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={userData.password}
+          onChange={handleChange}
+        />
+        {errors.password && <p className="error">{errors.password}</p>}
+        <input
+          className={errors.password ? "error-input" : ""}
+          type="text"
+          placeholder="NIC Number"
+          name="idCardNumber"
+          value={userData.idCardNumber}
+          onChange={handleChange}
+        />
+        {errors.idCardNumber && <p className="error">{errors.idCardNumber}</p>}
+
+        <input
+          className={errors.phoneNumber ? "error-input" : ""}
+          type="text"
+          placeholder="Phone Number"
+          name="phoneNumber"
+          value={userData.phoneNumber}
+          onChange={handleChange}
+        />
+        {errors.phoneNumber && <p className="error">{errors.phoneNumber}</p>}
+        <button onClick={handleSubmit}>Register</button>
+      </div>
     </div>
   );
 }
