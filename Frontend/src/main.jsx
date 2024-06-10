017f30a8ca88d1d5ea567efcc5087a9576495aa2
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-11 00:08:25 +0530

    Complete Register Page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Register from "./Pages/Register.jsx";
+import LoginPage from "./Pages/LoginPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Register />
+    <LoginPage />
   </React.StrictMode>
 );
