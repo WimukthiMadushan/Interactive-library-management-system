4e11d9c1f368e5521b23b39beccc12e47375fb7a
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-13 18:33:10 +0530

    remove unwanted services component

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./../Styles/HomePage.css";
 import Header from "../Components/Header";
-import Services from "../Components/Services";
+
 import Footer from "../Components/Footer";
 
 function HomePage() {
