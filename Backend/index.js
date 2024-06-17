d8f110c9ad32c9492924374a50d7bac037fc97ee
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-17 21:31:29 +0530

    create new Routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,23 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import authRoutes from "./Routes/Auth.js";
 import userRoutes from "./Routes/User.js";
 import bookRoutes from "./Routes/Book.js";
+import categoryRoutes from "./Routes/Category.js";
+import publisherRoutes from "./Routes/Publishers.js";
 
 const app = express();
 const port = 5000;
 app.use(express.json());
 app.use(bodyParser.json());
-
+app.use(cors());
 //Routes....
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/book", bookRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/publisher", publisherRoutes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
