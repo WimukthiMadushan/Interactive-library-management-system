956f9ab38c6a0a77b2f42cb44d060b592e0d2dd2
Wimukthi_madushan <bandarawgwm.21@uom.lk>
2024-06-19 13:19:03 +0530

    complete user registration

--- Backend/Controllers/Auth.js
+++ Backend/Controllers/Auth.js
@@ -3,7 +3,65 @@ import jwt from "jsonwebtoken";
 import connection from "./../DataBase.js";
 import bcrypt from "bcrypt";
 
-export const register = (req, res) => {};
+export const register = (req, res) => {
+  const {
+    First_Name,
+    Last_Name,
+    Username,
+    Password,
+    Email,
+    Address,
+    NIC,
+    Mobile,
+  } = req.body;
+  //validate requesr body
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  // Check if the user exists
+  connection.query(
+    "SELECT * FROM User WHERE Username = ?",
+    [Username],
+    (err, result) => {
+      if (err) {
+        console.error("Database error: ", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      if (result.length > 0) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      // Hash the password
+      const hashedPassword = bcrypt.hashSync(Password, 10);
+
+      // Insert the user into the database
+      connection.query(
+        "INSERT INTO User ( First_Name, Last_Name,Username, Password, Email,Address, NIC, Mobile, Registered_Date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          First_Name,
+          Last_Name,
+          Username,
+          hashedPassword,
+          Email,
+          Address,
+          NIC,
+          Mobile,
+          new Date().toISOString().slice(0, 19).replace("T", " "),
+        ],
+        (err, result) => {
+          if (err) {
+            console.error("Database error: ", err);
+            return res.status(500).json({ message: "Internal server error" });
+          }
+
+          // Assuming the user is successfully inserted
+          return res.status(201).json({ message: "User created successfully" });
+        }
+      );
+    }
+  );
+};
 
 export const login = (req, res) => {
   const { Username, Password } = req.body;
@@ -28,6 +86,7 @@ export const login = (req, res) => {
       }
 
       // Check if the password is correct
+      console.log(result);
       const user = result[0];
       if (!bcrypt.compareSync(Password, user.Password)) {
         return res
